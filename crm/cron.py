f3c066bd1b9a0974b53e02982ee9c103a1a9539e
keroojoseph0 <keroojoseph2@.com>
2026-01-05 14:45:45 +0200

    Fixed issue

--- crm/cron.py
+++ crm/cron.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-import requests
+from gql.transport.requests import RequestsHTTPTransport
+from gql import gql ,Client
 
 def log_crm_heartbeat():
     timestamp = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
@@ -22,3 +23,5 @@ def log_crm_heartbeat():
     except Exception:
         with open("/tmp/crm_heartbeat_log.txt", "a") as file:
             file.write(f"{timestamp} GraphQL endpoint NOT responsive\n")
+
+
---
+++ crm/settings.py
@@ -0,0 +1,8 @@
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
+
+
+INSTALLED_APPS = [
+    'django_crontab',
+]
